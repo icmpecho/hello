3386b56952dd0692b336816ab8dd1b1cb57f736b
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2015-08-25 00:45:28 +0700

    Create executable to call hello module

---
+++ cmd/hello/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "github.com/icmpecho/hello"
+)
+
+func main() {
+  name := "icmpecho"
+  if len(os.Args) >= 2 {
+    name = os.Args[1]
+  }
+  greet := hello.Greet(name)
+  fmt.Printf(greet)
+}
+
